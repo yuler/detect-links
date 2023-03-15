66983f7e24580a3c8e75cba560de29d0a012183a
yuler <is.yuler@gmail.com>
2023-03-15 12:55:58 +0800

    Update deploy job skip vitest dep

--- app/detect.server.test.ts
+++ app/detect.server.test.ts
@@ -1,5 +1,6 @@
 import { domainDetect } from "./detect.server";
 
+// TODO: fail in GitHub Action, maybe because of the network
 test("blocked domain `https://wx2142195ea9758f48-x.mp.zhongwenxiaoshuo.com/index.html` in WeChat", async () => {
   const result = await domainDetect(
     "https://wx2142195ea9758f48-x.mp.zhongwenxiaoshuo.com/index.html"
