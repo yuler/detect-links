40693fb5e9504313679145c67d356c3e67dacb16
yuler <is.yuler@gmail.com>
2023-03-15 14:28:32 +0800

    Fix start script

--- server.ts
+++ server.ts
@@ -48,8 +48,7 @@ app.all(
       }
 );
 
-const port = process.env.PORT || 3000;
-
+const port = process.env.PORT ?? 3000
 app.listen(port, () => {
   // require the built app so we're ready when the first request comes in
   require(BUILD_DIR);
